e61018d95f465ddec4ddf55d13f2e88938766679
thomasbockhorn <historian.thomas@gmail.com>
2023-12-29 04:42:59 -0600

    Add new file ebookstore-demo/resources/js/Components/Navbar/Navbar.vue for navigation test.

---
+++ ebookstore-demo/resources/js/Components/Navbar/Navbar.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                Navigation test
+            </div>
+        </nav>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
