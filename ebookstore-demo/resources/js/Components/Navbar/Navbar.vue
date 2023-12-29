4491108a7d856a05f375f313834b8f357e4a2af7
thomasbockhorn <historian.thomas@gmail.com>
2023-12-29 05:17:46 -0600

    Refactor Navbar component to include a menu icon with animation.

--- ebookstore-demo/resources/js/Components/Navbar/Navbar.vue
+++ ebookstore-demo/resources/js/Components/Navbar/Navbar.vue
@@ -1,5 +1,7 @@
 <script setup>
+    import { ref } from 'vue';
 
+    const openMenu = ref(false);
 </script>
 
 <template>
@@ -8,7 +10,10 @@
             <div class="container p-3">
                 <div class="flex justify-between items-center">
                     <input type = "text">
-                    <button>hamburger</button>
+                    <button class="menu-wrapper mb-2" @click="openMenu = !openMenu" :class="{ active: openMenu }">
+                        <span class="menu-bar one"></span>
+                        <span class="menu-bar two"></span>
+                    </button>
                 </div>
             </div>
         </nav>
@@ -16,5 +21,36 @@
 </template>
 
 <style scoped>
-
+.menu-wrapper{
+    width: 40px;
+    height: auto;
+    cursor: pointer;
+    position: relative;
+}
+.menu-bar{
+    position: absolute;
+    width: 100%;
+    height: 2px;
+    background-color: #000;
+    left: 0;
+}
+.one{
+    top: 0;
+    animation-delay: 0.1s;
+    transition: all 0.3s;
+}
+.two{
+    top: 10px;
+    transition: all 0.3s;
+}
+.active .one{
+    top: 50%;
+    left: 0;
+    transform: rotate(45deg);
+}
+.active .two{
+    top: 50%;
+    left: 0;
+    transform: rotate(-45deg);
+}
 </style>
