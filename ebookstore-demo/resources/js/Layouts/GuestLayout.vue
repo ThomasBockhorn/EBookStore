2508be3609675a699a2a6d6588ac3dfae6c31b0a
thomasbockhorn <historian.thomas@gmail.com>
2023-12-29 04:43:27 -0600

    Refactor GuestLayout.vue to import and use the Navbar component instead of ApplicationLogo.

--- ebookstore-demo/resources/js/Layouts/GuestLayout.vue
+++ ebookstore-demo/resources/js/Layouts/GuestLayout.vue
@@ -1,13 +1,14 @@
 <script setup>
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+    import Navbar from '../Components/Navbar/Navbar.vue'
 
 </script>
 
 <template>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-        <inertia-link href="/" />
-        <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
-
-        <slot />
+    <div>
+        <Navbar />
     </div>
 </template>
+
+<style scoped>
+
+</style>
