fdd7d1b2723923d4048fa072237badfbf07bcd4b
thomasbockhorn <historian.thomas@gmail.com>
2024-01-10 05:35:51 -0600

    Add Footer component to GuestLayout.vue.

---
+++ ebookstore-demo/resources/js/Components/Footer/Footer.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+</script>
+
+<template>
+    <footer>
+        <div class="bg-gray-800 text-white">
+            <div class="container mx-auto py-8">
+                <div class="flex flex-wrap">
+                    <div class="w-full md:w-1/4 text-center md:text-left">
+                        <h5 class="uppercase mb-6 font-bold">Links</h5>
+                        <ul class="mb-4">
+                            <li class="mt-2">
+                                <a href="#" class="hover:underline">FAQ</a>
+                            </li>
+                            <li class="mt-2">
+                                <a href="#" class="hover:underline">Help</a>
+                            </li>
+                            <li class="mt-2">
+                                <a href="#" class="hover:underline">Support</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<style scoped>
+
+</style>
--- ebookstore-demo/resources/js/Layouts/GuestLayout.vue
+++ ebookstore-demo/resources/js/Layouts/GuestLayout.vue
@@ -1,11 +1,14 @@
 <script setup>
     import Navbar from '../Components/Navbar/Navbar.vue'
+    import Footer from '../Components/Footer/Footer.vue'
 
 </script>
 
 <template>
     <div>
         <Navbar />
+        <slot />
+        <Footer />
     </div>
 </template>
 
