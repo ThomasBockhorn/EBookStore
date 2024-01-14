1f894256ebaf78658facd99e239c377e5c07f929
thomasbockhorn <historian.thomas@gmail.com>
2024-01-14 10:38:11 -0600

    Refactor search input bar styling for Navbar component.

--- ebookstore-demo/resources/js/Components/Navbar/Navbar.vue
+++ ebookstore-demo/resources/js/Components/Navbar/Navbar.vue
@@ -21,9 +21,9 @@
             <div v-if="openMenu" class="bg-white shadow-md">
                 <div class="flex flex-col items-center">
                     <!-- Make sure to add the list of links here -->
-                    <form class="flex flex-row border-black m-5">
-                        <input type = "text" name = "" id = "" class="grow-rows-1 border-0">
-                        <button type="submit"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <form class="flex flex-row border-black m-5 border-2 active:bg-blue-50">
+                        <input type = "text" name = "" id = "search" class="grow-rows-1 border-0 shadow-transparent appearance-none">
+                        <button type="submit"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 border-0">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                         </svg>
                         </button>
@@ -71,15 +71,18 @@
     transform: rotate(-45deg);
 }
 
-input:focus{
-    outline:none;
+// Search Input Bar Styling
+#search:active{
+    border: 0;
+    outline: 0;
+    appearance: none;
+    box-shadow: none;
 }
 
-form:focus-within{
-    outline: 1px solid blue;
-}
-
-form{
-    border: 1px solid black;
+#search:focus{
+    border: 0;
+    outline: 0;
+    appearance: none;
+    box-shadow: none;
 }
 </style>
