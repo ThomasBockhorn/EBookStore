a8ad4d438516a76929f468dffa04d73df12da6ff
thomasbockhorn <historian.thomas@gmail.com>
2024-01-14 20:20:12 -0600

    Add SearchInput component to Navbar.vue.

--- ebookstore-demo/resources/js/Components/Navbar/Navbar.vue
+++ ebookstore-demo/resources/js/Components/Navbar/Navbar.vue
@@ -1,5 +1,6 @@
 <script setup>
     import { ref } from 'vue';
+    import SearchInput from '../Navbar/SearchInput.vue';
 
     const openMenu = ref(false);
 </script>
@@ -20,14 +21,7 @@
        <div>
             <div v-if="openMenu" class="bg-white shadow-md">
                 <div class="flex flex-col items-center">
-                    <form class="flex flex-row border-black m-5 border-2 active:bg-blue-50">
-                        <input type= "text" name="" id="search" class="grow-rows-1 border-0">
-                        <button type="submit">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 border-0">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                            </svg>
-                        </button>
-                    </form>
+                    <SearchInput />
                     <a href="#" class="p-3">Home</a>
                     <a href="#" class="p-3">About</a>
                     <a href="#" class="p-3">Contact</a>
@@ -69,18 +63,5 @@
     top: 50%;
     left: 0;
     transform: rotate(-45deg);
-}
-
-#search:active{
-    border: 0;
-    outline: 0;
-    appearance: none;
-    box-shadow: none;
-}
-#search:focus{
-    border: 0;
-    outline: 0;
-    appearance: none;
-    box-shadow: none;
 }
 </style>
---
+++ ebookstore-demo/resources/js/Components/Navbar/SearchInput.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+
+<template>
+    <form class="flex flex-row border-black m-5 border-2 active:bg-blue-50">
+        <input type= "text" name="" id="search" class="grow-rows-1 border-0">
+        <button type="submit">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 border-0">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg>
+        </button>
+    </form>
+</template>
+
+<style scoped>
+#search:active{
+    border: 0;
+    outline: 0;
+    appearance: none;
+    box-shadow: none;
+}
+#search:focus{
+    border: 0;
+    outline: 0;
+    appearance: none;
+    box-shadow: none;
+}
+</style>
