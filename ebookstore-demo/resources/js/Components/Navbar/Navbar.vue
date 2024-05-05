c80875af2961dd0323e0209d66bbd523f2cdcdb9
thomasbockhorn <historian.thomas@gmail.com>
2024-05-04 22:10:45 -0500

    Added EBookStore name

--- ebookstore-demo/resources/js/Components/Navbar/Navbar.vue
+++ ebookstore-demo/resources/js/Components/Navbar/Navbar.vue
@@ -3,7 +3,7 @@
         class="lg:px-16 px-4 bg-white flex flex-wrap items-center py-4 shadow-md"
     >
         <div class="flex-1 flex justify-between items-center">
-            <a href="#" class="text-xl">Company</a>
+            <a href="#" class="text-xl">EBookStore</a>
         </div>
 
         <label for="menu-toggle" class="pointer-cursor md:hidden block">
