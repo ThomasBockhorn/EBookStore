1ec7bf1f784a7f4f42d63504bfbd6bc33ff2cbb9
thomasbockhorn <historian.thomas@gmail.com>
2024-05-11 12:28:13 -0500

    Added NavbarNav and Hamburger components

--- ebookstore-demo/resources/js/Components/Navbar/Navbar.vue
+++ ebookstore-demo/resources/js/Components/Navbar/Navbar.vue
@@ -8,61 +8,19 @@
 
         <label for="menu-toggle" class="pointer-cursor md:hidden block">
             <button @click="toggleSidebar">
-                <svg
-                    class="fill-current text-gray-900"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    viewBox="0 0 20 20"
-                >
-                    <title>menu</title>
-                    <path
-                        d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"
-                    ></path>
-                </svg>
+                <Hamburger />
             </button>
         </label>
         <div class="hidden md:flex md:items-center md:w-auto w-full" id="menu">
-            <nav>
-                <ul
-                    class="md:flex items-center justify-between text-base text-gray-700 pt-4 md:pt-0"
-                >
-                    <li>
-                        <a class="md:p-4 py-3 px-0 block" href="#">AboutUs</a>
-                    </li>
-                    <li><a class="md:p-4 py-3 px-0 block" href="#">Blog</a></li>
-                    <li>
-                        <a class="md:p-4 py-3 px-0 block md:mb-0 mb-2" href="#"
-                            >Contact Us</a
-                        >
-                    </li>
-                    <li>
-                        <a class="md:p-4 py-3 px-0 block md:mb-0 mb-2" href="#">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="20"
-                                height="20"
-                                fill="currentColor"
-                                class="bi bi-bag-plus"
-                                viewBox="0 0 16 16"
-                            >
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
-                                />
-                                <path
-                                    d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"
-                                />
-                            </svg>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
+            <NavbarNav />
         </div>
     </header>
 </template>
 
 <script>
+import NavbarNav from "./NavbarNav.vue";
+import Hamburger from "./NavbarComponents/Hamburger.vue";
+
 export default {
     data() {
         return {
@@ -74,6 +32,10 @@ export default {
             this.showSidebar = !this.showSidebar;
             this.$emit("toggle-sidebar", this.showSidebar);
         },
+    },
+    components: {
+        NavbarNav,
+        Hamburger,
     },
 };
 </script>
