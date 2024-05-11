db826c9a79e3982d9d4922bfd6732c95a14d5018
thomasbockhorn <historian.thomas@gmail.com>
2024-05-11 12:41:39 -0500

    Got rid of unnecessary comments

--- ebookstore-demo/resources/js/Pages/Welcome.vue
+++ ebookstore-demo/resources/js/Pages/Welcome.vue
@@ -1,7 +1,5 @@
 <template>
     <GuestLayout>
-        <!-- source: https://github.com/mfg888/Responsive-Tailwind-CSS-Grid/blob/main/index.html -->
-
         <div class="text-center p-10">
             <h1 class="font-bold text-4xl mb-4">Welcome to EBookStore</h1>
             <h1 class="text-3xl">Pick your next read</h1>
