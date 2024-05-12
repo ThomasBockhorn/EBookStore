6182148a5b8ebe1e624a11d28976f24dbbd4f703
thomasbockhorn <historian.thomas@gmail.com>
2024-05-12 08:49:29 -0500

    converted SidebarNav to composition

--- ebookstore-demo/resources/js/Components/Sidebar/SidebarNav.vue
+++ ebookstore-demo/resources/js/Components/Sidebar/SidebarNav.vue
@@ -68,14 +68,6 @@
     </nav>
 </template>
 
-<script>
+<script setup>
 import ShoppingCart from "../GlobalComponents/ShoppingCart/ShoppingCart.vue";
-
-export default {
-    components: {
-        ShoppingCart,
-    },
-};
 </script>
-
-<style></style>
