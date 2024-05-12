0e412b6b5f8d521834b6f4966141a2e13f746d14
thomasbockhorn <historian.thomas@gmail.com>
2024-05-12 11:53:23 -0500

    Copied Navbar to backend component

---
+++ ebookstore-demo/resources/js/Components/Backend/Navbar/NavbarNav.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav>
+        <ul
+            class="md:flex items-center justify-between text-base text-gray-700 pt-4 md:pt-0"
+        >
+            <li>
+                <a class="md:p-4 py-3 px-0 block" href="#">AboutUs</a>
+            </li>
+            <li><a class="md:p-4 py-3 px-0 block" href="#">Blog</a></li>
+            <li>
+                <a class="md:p-4 py-3 px-0 block md:mb-0 mb-2" href="#"
+                    >Contact Us</a
+                >
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script setup></script>
