50a55b9f263b9277fec00b79aa2ab66ad2e81563
thomasbockhorn <historian.thomas@gmail.com>
2024-05-12 11:53:02 -0500

    copied Footer to Backend component folder

---
+++ ebookstore-demo/resources/js/Components/Backend/Footer/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer class="bg-sky-400">
+        <div
+            class="mx-auto max-w-7xl py-8 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8"
+        >
+            <FooterNav />
+            <FooterSocialMedia />
+            <div class="mt-8 md:order-1 md:mt-0">
+                <p class="text-center text-base text-white">
+                    &copy; Thomas Bockhorn.
+                </p>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script setup>
+import FooterNav from "./FooterNav.vue";
+import FooterSocialMedia from "./FooterSocialMedia.vue";
+</script>
