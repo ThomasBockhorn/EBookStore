0f45a47dd2519835c8ad31c92edae1b996929341
thomasbockhorn <historian.thomas@gmail.com>
2024-05-12 11:53:40 -0500

    Copied Sidebar to backend component

---
+++ ebookstore-demo/resources/js/Components/Backend/Sidebar/Sidebar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="md:hidden flex-col w-64 bg-gray-800">
+        <div class="flex items-center justify-center h-16 bg-gray-900">
+            <span class="text-white font-bold uppercase">EBookStore</span>
+            <label for="menu-toggle" class="pointer-cursor block"> </label>
+        </div>
+        <div class="flex flex-col flex-1 overflow-y-auto">
+            <SidebarNav />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import SidebarNav from "./SidebarNav.vue";
+</script>
