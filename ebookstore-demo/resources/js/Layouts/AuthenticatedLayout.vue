671144477d9f588ebdb08244c67c1cf3432fe5f2
thomasbockhorn <historian.thomas@gmail.com>
2024-05-12 11:58:23 -0500

    AuthenticatedLayout converted to composition

--- ebookstore-demo/resources/js/Layouts/AuthenticatedLayout.vue
+++ ebookstore-demo/resources/js/Layouts/AuthenticatedLayout.vue
@@ -7,26 +7,15 @@
     </div>
 </template>
 
-<script>
+<script setup>
 import Navbar from "../Components/Backend/Navbar/Navbar.vue";
 import Footer from "../Components/Backend/Footer/Footer.vue";
 import Sidebar from "../Components/Backend/Sidebar/Sidebar.vue";
+import { ref } from "vue";
 
-export default {
-    components: {
-        Navbar,
-        Footer,
-        Sidebar,
-    },
-    data() {
-        return {
-            show: false,
-        };
-    },
-    methods: {
-        showSidebar(value) {
-            this.show = value;
-        },
-    },
+const show = ref(false);
+
+const showSidebar = (value) => {
+    show.value = value;
 };
 </script>
