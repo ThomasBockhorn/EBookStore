e17d3e5f6431c28634f46db9724c34e541627397
thomasbockhorn <historian.thomas@gmail.com>
2024-05-15 04:03:11 -0500

    Added a link to the main site

--- ebookstore-demo/resources/js/Components/Frontend/Navbar/Navbar.vue
+++ ebookstore-demo/resources/js/Components/Frontend/Navbar/Navbar.vue
@@ -3,7 +3,7 @@
         class="lg:px-16 px-4 bg-white flex flex-wrap items-center py-4 shadow-md"
     >
         <div class="flex-1 flex justify-between items-center">
-            <a href="#" class="text-xl">EBookStore</a>
+            <Link href="/" class="text-xl">EBookStore</Link>
         </div>
 
         <label for="menu-toggle" class="pointer-cursor md:hidden block">
@@ -20,6 +20,7 @@
 <script setup>
 import NavbarNav from "./NavbarNav.vue";
 import Hamburger from "@/Components/GlobalComponents/NavbarComponents/Hamburger.vue";
+import { Link } from "@inertiajs/vue3";
 import { ref } from "vue";
 
 const sideBar = ref(false);
