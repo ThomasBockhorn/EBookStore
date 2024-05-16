bd850f544e1a3522c923034b49433dc3d948ce7b
thomasbockhorn <historian.thomas@gmail.com>
2024-05-15 22:42:43 -0500

    Created AuthLayout

---
+++ ebookstore-demo/resources/js/Layouts/AuthLayout.vue
@@ -0,0 +1,20 @@
+<template>
+    <div
+        class="dark:bg-gradient-to-l from-gray-900 to-gray-600 flex justify-center items-center w-screen h-screen p-5"
+    >
+        <div
+            class="bg-white shadow-md dark:shadow-gray-600 rounded px-8 pt-6 pb-8 mb-4 flex flex-col w-full md:w-1/3 dark:bg-gray-800"
+        >
+            <h1
+                class="text-2xl font-semibold mb-4 text-center text-gray-900 dark:text-gray-200"
+            >
+                Admin Login
+            </h1>
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script></script>
+
+<style></style>
