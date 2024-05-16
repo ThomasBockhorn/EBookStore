98a355187da50023e87f1eaf9a4112210e6ac2a7
thomasbockhorn <historian.thomas@gmail.com>
2024-05-16 11:24:24 -0500

    Added a wave styling to welcome and sky-500

--- ebookstore-demo/resources/js/Pages/Welcome.vue
+++ ebookstore-demo/resources/js/Pages/Welcome.vue
@@ -1,11 +1,16 @@
 <template>
     <GuestLayout>
-        <div class="text-center p-10 box mb-5">
-            <h1 class="font-bold text-4xl mb-4 text-white">
-                Welcome to EBookStore
-            </h1>
+        <div class="text-center p-10 bg-sky-500">
+            <h1 class="font-bold text-4xl text-white">Welcome to EBookStore</h1>
             <h1 class="text-3xl text-white">Pick your next read</h1>
         </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path
+                fill="#08A6F7"
+                fill-opacity="1"
+                d="M0,96L48,106.7C96,117,192,139,288,149.3C384,160,480,160,576,149.3C672,139,768,117,864,90.7C960,64,1056,32,1152,21.3C1248,11,1344,21,1392,26.7L1440,32L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+            ></path>
+        </svg>
         <Search />
         <section
             id="Projects"
@@ -24,13 +29,4 @@ import Search from "@/Components/Frontend/Search/Search.vue";
 import Book from "@/Components/Frontend/Books/Book.vue";
 </script>
 
-<style scoped>
-.box {
-    background: rgb(8, 166, 247);
-    background: linear-gradient(
-        90deg,
-        rgba(8, 166, 247, 1) 51%,
-        rgba(8, 47, 247, 1) 100%
-    );
-}
-</style>
+<style scoped></style>
