4d7253ddb96f606a98d381eefd8f7b20a881c1e5
thomasbockhorn <historian.thomas@gmail.com>
2024-05-25 08:30:13 -0500

    Added links to NavbarWithSearch

--- ebookstore-demo/resources/js/Components/Frontend/Navbar/NavbarWithSearch.vue
+++ ebookstore-demo/resources/js/Components/Frontend/Navbar/NavbarWithSearch.vue
@@ -122,27 +122,34 @@
                     class="flex flex-col p-4 md:p-0 mt-4 font-medium rounded-lg bg-sky-500 md:space-x-8 md:flex-row md:mt-0 md:border-0 md:bg-sky-500"
                 >
                     <li>
-                        <a
-                            href="#"
+                        <Link
+                            href="/aboutus"
                             class="block py-2 px-3 text-white bg-sky-700 rounded md:bg-transparent md:text-blue-700 md:p-0"
                             aria-current="page"
-                            >Home</a
+                            >About Us</Link
                         >
                         <!--bg-blue-700 if active-->
                     </li>
                     <li>
-                        <a
-                            href="#"
+                        <Link
+                            href="/blog"
                             class="block py-2 px-3 text-white rounded hover:bg-sky-300 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
-                            >About</a
+                            >Blog</Link
                         >
                     </li>
                     <li>
-                        <a
-                            href="#"
+                        <Link
+                            href="/contactus"
                             class="block py-2 px-3 text-white rounded hover:bg-sky-300 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
-                            >Services</a
+                            >Contact Us</Link
                         >
+                    </li>
+                    <li>
+                        <Link
+                            href="/shoppingcart"
+                            class="block py-2 px-3 text-white rounded hover:bg-sky-300 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
+                            ><ShoppingCart
+                        /></Link>
                     </li>
                 </ul>
             </div>
@@ -151,6 +158,7 @@
 </template>
 
 <script setup>
+import ShoppingCart from "@/Components/GlobalComponents/ShoppingCart/ShoppingCart.vue";
 import { Link } from "@inertiajs/vue3";
 import { ref } from "vue";
 </script>
