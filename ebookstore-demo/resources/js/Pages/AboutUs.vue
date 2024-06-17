f1ab30d085aba0f8416f0d8b5f855cdb561d80b1
thomasbockhorn <historian.thomas@gmail.com>
2024-06-17 04:32:59 -0500

    Added header to AboutUs page

--- ebookstore-demo/resources/js/Pages/AboutUs.vue
+++ ebookstore-demo/resources/js/Pages/AboutUs.vue
@@ -1,11 +1,17 @@
 <template>
     <GuestLayout>
-        <h1>About Us</h1>
+        <section>
+            <div class="text-center bg-sky-500">
+                <h1 class="font-bold text-4xl text-white">About Us</h1>
+            </div>
+            <WaveHeader />
+        </section>
     </GuestLayout>
 </template>
 
 <script setup>
 import GuestLayout from "@/Layouts/GuestLayout.vue";
+import WaveHeader from "@/Components/GlobalComponents/WaveHeader/WaveHeader.vue";
 </script>
 
 <style></style>
