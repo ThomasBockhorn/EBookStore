ae71385e2da909ef42fc83b909ea01d9b9d4ae74
thomasbockhorn <historian.thomas@gmail.com>
2024-06-18 04:27:24 -0500

    Creating categories in welcome section

--- ebookstore-demo/resources/js/Pages/Welcome.vue
+++ ebookstore-demo/resources/js/Pages/Welcome.vue
@@ -5,19 +5,19 @@
                 <h1 class="font-bold text-4xl text-white p-5">Welcome</h1>
             </div>
         </section>
-        <div
-            id="sales"
-            class="text-center border-2 border-black m-5 bg-slate-200 rounded-lg shadow-lg"
+        <section
+            id="Projects"
+            class="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5"
         >
-            <h2 class="font-bold text-2xl text-black p-5">Sales</h2>
-            <section
-                id="Projects"
-                class="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5"
+            <div
+                id="sales"
+                class="text-center border-2 border-black m-5 bg-slate-200 rounded-lg shadow-lg"
             >
+                <h2 class="font-bold text-2xl text-black p-5">Sales</h2>
                 <Book />
-            </section>
-            <pagination />
-        </div>
+            </div>
+        </section>
+        <pagination />
     </GuestLayout>
 </template>
 
