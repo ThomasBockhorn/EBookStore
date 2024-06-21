d2fb00dd9f98b77863695438280e47bcbf4e0f57
thomasbockhorn <historian.thomas@gmail.com>
2024-06-20 21:12:04 -0500

    Fixed tailwind issues with smaller viewports in about us page

--- ebookstore-demo/resources/js/Pages/AboutUs.vue
+++ ebookstore-demo/resources/js/Pages/AboutUs.vue
@@ -5,47 +5,45 @@
                 <h1 class="font-bold text-4xl text-white p-5">About Us</h1>
             </div>
         </section>
-            <section class="grid grid-cols-2">
-                <div class="m-5">
-                    <img src="/img/books-1842261_1280.jpg" alt="bookstore" class="rounded">
-                </div>
-                <div>
-                    <p class="m-5">
-                        <strong>Welcome to EBookStore</strong>, your ultimate destination for a world of digital reading!
-                        At eBook Haven, we believe in the transformative power of books. Our mission is to make literature accessible,
-                        engaging, and enjoyable for everyone, everywhere. Whether you're a voracious reader, a casual browser,
-                        or a curious explorer, we have something special for you.
-                    </p>
-                    <p class="m-5">
-                        <strong>Who We Are</strong>
-                        eBook Haven was founded by a group of passionate book lovers and tech enthusiasts who saw the potential of
-                        digital platforms to revolutionize reading. Our team is dedicated to curating an extensive collection of
-                        ebooks across various genres, ensuring that every reader finds their perfect match.
-                    </p>
-                    <p class="m-5">
-                        <strong>Our Collection</strong>
-                        From timeless classics to contemporary bestsellers, from riveting mysteries to insightful non-fiction,
-                        our diverse library caters to all tastes and preferences. We also pride ourselves on supporting
-                        independent authors and emerging voices, offering readers unique stories and fresh perspectives.
-                    </p>
-                    <p class="m-5">
-                        <strong>Why Choose Us?</strong>
-                        <ul>
-                            <li class="ml-9"><strong>Vast Selection:</strong> Explore thousands of titles from renowned publishers and indie authors alike.</li>
-                            <li class="ml-9"><strong>User-Friendly Experience:</strong> Our intuitive platform makes browsing, purchasing, and reading a breeze.</li>
-                            <li class="ml-9"><strong>Affordable Prices:</strong> Enjoy competitive pricing and frequent discounts, making reading affordable.</li>
-                            <li class="ml-9"><strong>Community Focus:</strong> Join our community of readers, participate in discussions, and share your love for books.</li>
-                         </ul>
-                    </p>
-                    <p class="m-5">
-                        <strong>Our Commitment</strong>
-                        We are committed to promoting a culture of reading and lifelong learning. We continuously update our catalog, enhance our
-                        platform, and listen to our customers' feedback to provide the best possible experience. At eBook Haven, your satisfaction is our top priority.
-                    </p>
-                    <p class="m-5">
-                        Thank you for choosing eBook Haven. Dive into your next great read with us!
-                    </p>
-                </div>
+        <section class="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2 gap-5">
+             <img src="/img/books-1842261_1280.jpg" alt="bookstore" class="rounded mt-5">
+            <div>
+                <p class="m-5">
+                    <strong>Welcome to EBookStore</strong>, your ultimate destination for a world of digital reading!
+                    At eBook Haven, we believe in the transformative power of books. Our mission is to make literature accessible,
+                    engaging, and enjoyable for everyone, everywhere. Whether you're a voracious reader, a casual browser,
+                    or a curious explorer, we have something special for you.
+                </p>
+                <p class="m-5">
+                    <strong>Who We Are</strong>
+                    eBook Haven was founded by a group of passionate book lovers and tech enthusiasts who saw the potential of
+                    digital platforms to revolutionize reading. Our team is dedicated to curating an extensive collection of
+                    ebooks across various genres, ensuring that every reader finds their perfect match.
+                </p>
+                <p class="m-5">
+                    <strong>Our Collection</strong>
+                    From timeless classics to contemporary bestsellers, from riveting mysteries to insightful non-fiction,
+                    our diverse library caters to all tastes and preferences. We also pride ourselves on supporting
+                    independent authors and emerging voices, offering readers unique stories and fresh perspectives.
+                </p>
+                <p class="m-5">
+                    <strong>Why Choose Us?</strong>
+                    <ul>
+                        <li class="ml-9"><strong>Vast Selection:</strong> Explore thousands of titles from renowned publishers and indie authors alike.</li>
+                        <li class="ml-9"><strong>User-Friendly Experience:</strong> Our intuitive platform makes browsing, purchasing, and reading a breeze.</li>
+                        <li class="ml-9"><strong>Affordable Prices:</strong> Enjoy competitive pricing and frequent discounts, making reading affordable.</li>
+                        <li class="ml-9"><strong>Community Focus:</strong> Join our community of readers, participate in discussions, and share your love for books.</li>
+                        </ul>
+                </p>
+                <p class="m-5">
+                    <strong>Our Commitment</strong>
+                    We are committed to promoting a culture of reading and lifelong learning. We continuously update our catalog, enhance our
+                    platform, and listen to our customers' feedback to provide the best possible experience. At eBook Haven, your satisfaction is our top priority.
+                </p>
+                <p class="m-5">
+                    Thank you for choosing eBook Haven. Dive into your next great read with us!
+                </p>
+            </div>
         </section>
     </GuestLayout>
 </template>
