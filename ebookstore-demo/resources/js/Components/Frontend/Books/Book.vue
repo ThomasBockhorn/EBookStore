0518e749def9c1469ce178d630d68d3d4fa4724a
thomasbockhorn <historian.thomas@gmail.com>
2024-06-21 20:45:18 -0500

    Added some dummy info to those books

--- ebookstore-demo/resources/js/Components/Frontend/Books/Book.vue
+++ ebookstore-demo/resources/js/Components/Frontend/Books/Book.vue
@@ -4,26 +4,25 @@
     >
         <a href="#">
             <img
-                src="https://images.unsplash.com/photo-1649261191606-cb2496e97eee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-                alt="Product"
+                src="/img/night-4028339_1280.jpg"
                 class="h-80 w-25 object-cover rounded-t-xl"
             />
             <div class="px-4 py-3 w-25">
-                <span class="text-gray-400 mr-3 uppercase text-xs">Brand</span>
+                <span class="text-gray-400 mr-3 uppercase text-xs">Haiku</span>
                 <p
                     class="text-lg font-bold text-black truncate block capitalize"
                 >
-                    Product Name
+                    Moonlit Night
                 </p>
                 <div class="flex items-center">
                     <p
                         class="text-lg font-semibold text-black cursor-auto my-3"
                     >
-                        $149
+                        $15.99
                     </p>
                     <del>
                         <p class="text-sm text-gray-600 cursor-auto ml-2">
-                            $199
+                            $25.00
                         </p>
                     </del>
                     <div class="ml-auto">
