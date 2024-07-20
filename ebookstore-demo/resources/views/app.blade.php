a07b3d0bbc0b12e413497aac44b5dd1aeb95f248
thomasbockhorn <historian.thomas@gmail.com>
2024-07-20 16:21:55 -0500

    allowed the scroll bar to display

--- ebookstore-demo/resources/views/app.blade.php
+++ ebookstore-demo/resources/views/app.blade.php
@@ -15,16 +15,6 @@
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
         <style>
-            /* hide scrollbar but allow scrolling */
-            element {
-                -ms-overflow-style: none; /* for Internet Explorer, Edge */
-                scrollbar-width: none; /* for Firefox */
-                overflow-y: scroll;
-            }
-            element::-webkit-scrollbar {
-                display: none; /* for Chrome, Safari, and Opera */
-            }
-
             html{
                 scroll-behavior: smooth;
             }
