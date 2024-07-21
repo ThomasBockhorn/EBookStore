8534e96f9239383aa20c4671d57d2cd4bd8f85ed
thomasbockhorn <historian.thomas@gmail.com>
2024-07-21 10:05:00 -0500

    made the navbar sticky, white background, and z-index 50

--- ebookstore-demo/resources/js/Components/Frontend/Navbar/NavbarNav.vue
+++ ebookstore-demo/resources/js/Components/Frontend/Navbar/NavbarNav.vue
@@ -1,6 +1,6 @@
 <template>
     <!--Nav-->
-    <nav id="header" class="w-full z-30 top-0 py-1">
+    <nav id="header" class="w-full z-30 top-0 py-1 sticky bg-white z-50">
         <div
             class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-6 py-3"
         >
