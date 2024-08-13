0de00f77e01f14f78bc2ca0a9ef84b7485e4c124
thomasbockhorn <historian.thomas@gmail.com>
2024-08-13 00:40:19 -0500

    Created a category model with factory

--- ebookstore-demo/database/seeders/DatabaseSeeder.php
+++ ebookstore-demo/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Book;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         Book::factory(10)->create();
+        Category::factory(10)->create();
     }
 }
