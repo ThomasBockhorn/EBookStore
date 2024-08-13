a7c59ecae61e9175fb765158848f9a84f3a06353
thomasbockhorn <historian.thomas@gmail.com>
2024-08-12 21:51:24 -0500

    Created a MarketControls component and passed boolean to hide or show

--- ebookstore-demo/resources/js/Components/Frontend/Market/Sales.vue
+++ ebookstore-demo/resources/js/Components/Frontend/Market/Sales.vue
@@ -1,54 +1,7 @@
 <template>
     <section class="bg-white py-8" id="Sales">
         <div class="container mx-auto flex items-center flex-wrap pt-4 pb-12">
-            <nav id="store" class="w-full z-30 top-0 px-6 py-1">
-                <div
-                    class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-3"
-                >
-                    <a
-                        class="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl"
-                        href="#"
-                    >
-                        Sales
-                    </a>
-
-                    <div class="flex items-center" id="store-nav-content">
-                        <button
-                            class="pl-3 inline-block no-underline hover:text-black"
-                            @click="toDisplay = !toDisplay"
-                        >
-                            <svg
-                                class="fill-current hover:text-black"
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                            >
-                                <path
-                                    d="M7 11H17V13H7zM4 7H20V9H4zM10 15H14V17H10z"
-                                />
-                            </svg>
-                        </button>
-
-                        <a
-                            class="pl-3 inline-block no-underline hover:text-black"
-                            href="#"
-                        >
-                            <svg
-                                class="fill-current hover:text-black"
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                            >
-                                <path
-                                    d="M10,18c1.846,0,3.543-0.635,4.897-1.688l4.396,4.396l1.414-1.414l-4.396-4.396C17.365,13.543,18,11.846,18,10 c0-4.411-3.589-8-8-8s-8,3.589-8,8S5.589,18,10,18z M10,4c3.309,0,6,2.691,6,6s-2.691,6-6,6s-6-2.691-6-6S6.691,4,10,4z"
-                                />
-                            </svg>
-                        </a>
-                    </div>
-                </div>
-            </nav>
+            <MarketControls @isDisplay="displayBooks" />
             <div
                 class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-3"
                 :class="{ hidden: toDisplay }"
@@ -63,8 +16,14 @@
 </template>
 
 <script setup>
-import Book from "@/Components/Frontend/Books/Book.vue";
 import { ref } from "vue";
+import Book from "@/Components/Frontend/Books/Book.vue";
+import MarketControls from "@/Components/Frontend/Market/MarketControls/MarketControls.vue";
 
-let toDisplay = ref(false);
+const toDisplay = ref(false);
+
+const displayBooks = (displayBooks) => {
+    toDisplay.value = displayBooks;
+    console.log(toDisplay.value);
+};
 </script>
